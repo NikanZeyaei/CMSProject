8fa9592988e5169a05867b08cf5af49eec0b0957
Nikan Zeyaei <nikanzeyaei@gmail.com>
2021-11-01 16:18:30 +0330

    Made a basic index page

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,34 @@
 import express from 'express';
+import articleRouter from './routes/articles';
+import path from 'path';
 
 const app = express();
 
 app.set('view engine', 'ejs');
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.use(articleRouter);
+
 app.get('/', (req, res) => {
-  res.render('index');
+  const posts: any[] = [];
+  posts.push({
+    title: 'This is my first post',
+    description:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae sunt sed laborum necessitatibus voluptatum amet officiis libero deleniti alias aperiam.',
+    date: new Date().toLocaleString(),
+    id: 1,
+  });
+  posts.push({
+    title: 'This is my Second post',
+    description:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae sunt sed laborum necessitatibus voluptatum amet officiis libero deleniti alias aperiam.',
+    date: new Date().toLocaleString(),
+    id: 2,
+  });
+  res.render('index', { posts: posts });
 });
 
 app.listen(3000, () => {
